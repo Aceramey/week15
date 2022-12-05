aaa97da197a9e084aa9374e1b037710dcdbaed92
Aceramey <aceramey02@gmail.com>
2022-12-04 19:41:07 -0500

    added try and catch blocks

--- week15/part1.cpp
+++ week15/part1.cpp
@@ -8,7 +8,33 @@ class invalidRangeException{};
 
 char character(char start, int offset);
 
+char character(char start, int offset)
+{
+	
+}
+
 int main()
 {
+	char start = ' ';
+	int offset = 0;
+	bool done = false;
+	while (!done)
+	{
+		cout << "Enter the start character: ";
+		cin >> start;
+		cout << "Enter the offset: ";
+		cin >> offset;
+		try
+		{
+			cout << character(start, offset) << endl;
+		}
+		catch(invalidCharacterExcpeption e)
+		{
 
+		}
+		catch (invalidRangeException e)
+		{
+
+		}
+	}
 }
